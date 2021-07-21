55a8609f52cc74516d8ba299febf9ca87b51d5da
DIVYANSHURAWAT1 <87772362+DIVYANSHURAWAT1@users.noreply.github.com>
2021-07-21 21:21:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+
+var a = prompt('Enter the first variable: ');
+var b = prompt("enter the second number ")
+function setup(){
+  var b2 = createButton("click here to Swap");
+b2.position(200,300)
+b2.mousePressed(swap)
+
+  }
+
+
+function draw()
+{
+}
+function swap(){
+ [a,b]=[b,a]
+ console.log('new value of a '+ a)
+ console.log('new value of b '+ b)
+}
